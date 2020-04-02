5513524ee2d2a1cc8fa59f46b42ea51e34734985
Matthias Klass <matthias.klass@tngtech.com>
2020-04-02 07:57:49 +0200

    Don't log participants username if not enabled

--- server/Config.ts
+++ server/Config.ts
@@ -19,7 +19,7 @@ export class Config {
   public readonly sessionSecret = process.env.SESSION_SECRET || uuid();
   public readonly cookieMaxAgeMs = parseInt(process.env.COOKIE_MAX_AGE_MS || `${DAYS_30_MS}`, 10);
   public readonly disableAuthOnApi = !!process.env.DISABLE_AUTH_ON_API;
-  public readonly enableParticipantLogging = !!process.env.ENABLE_PARTICIPANT_LOGGING;
+  public readonly enableParticipantLogging = process.env.ENABLE_PARTICIPANT_LOGGING === "true";
 
   constructor() {}
 
