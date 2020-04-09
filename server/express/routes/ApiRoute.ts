c06467b1dcb531107e32e0d5990d96526003c951
Matthias Klass <matthias.klass@tngtech.com>
2020-04-09 10:04:03 +0200

    Log who replaced the room data

--- server/express/routes/ApiRoute.ts
+++ server/express/routes/ApiRoute.ts
@@ -5,6 +5,7 @@ import { MonitoringRoute } from "./MonitoringRoute";
 import { RoomsService } from "../../services/RoomsService";
 import ensureLoggedIn, { AuthenticatedRequest } from "../middleware/ensureLoggedIn";
 import { ZoomUsWebHookRoute } from "./ZoomUsWebHookRoute";
+import { logger } from "../../log";
 
 @Service()
 export class ApiRoute implements ExpressRoute {
@@ -40,7 +41,13 @@ export class ApiRoute implements ExpressRoute {
       this.roomsService.leaveRoom(req.params.roomId, req.params.userId);
       res.sendStatus(200);
     });
-    router.post("/admin/replaceAllRooms", ensureLoggedIn, (req, res) => {
+    router.post("/admin/replaceAllRooms", ensureLoggedIn, (req: AuthenticatedRequest, res) => {
+      logger.info({
+        message: "replacing all rooms data",
+        user: req.currentUser.name,
+        email: req.currentUser.email,
+        data: req.body,
+      });
       this.roomsService.replaceRoomsWith(req.body);
       res
         .json({
