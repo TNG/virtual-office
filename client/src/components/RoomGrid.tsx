230d3929c2cf722f5d161dd04852c195b18a589b
Fabian Pecher <fabian.pecher@tngtech.com>
2020-04-24 17:00:33 +0200

    Add group join card to groups with enabled option

--- client/src/components/RoomGrid.tsx
+++ client/src/components/RoomGrid.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
+import { partition } from "lodash";
 
-import RoomCard from "./RoomCard";
-import { RoomWithParticipants } from "../../../server/express/types/RoomWithParticipants";
-import theme from "../theme";
 import { Group } from "../../../server/express/types/Group";
+import { RoomWithParticipants } from "../../../server/express/types/RoomWithParticipants";
+import GroupJoinCard from "./GroupJoinCard";
+import RoomCard from "./RoomCard";
+import theme from "../theme";
 
 const useStyles = makeStyles<typeof theme>((theme) => ({
   title: {
     margin: 12,
-    marginTop: 16,
-    marginBottom: 8,
+    marginTop: 24,
     padding: 0,
   },
   grid: {
@@ -39,16 +40,43 @@ const useStyles = makeStyles<typeof theme>((theme) => ({
 const RoomGrid = ({ group, rooms }: { group: Group; rooms: RoomWithParticipants[] }) => {
   const classes = useStyles();
 
+  function renderGridCard(key: string, card: any) {
+    return (
+      <Box key={key} className={classes.card}>
+        {card}
+      </Box>
+    );
+  }
+
+  function selectShownRooms() {
+    if (!group.groupJoin) {
+      return rooms;
+    }
+
+    const [emptyRooms, filledRooms] = partition(rooms, (room) => room.participants.length === 0);
+    return [...filledRooms, ...emptyRooms.slice(0, 1)];
+  }
+
+  function renderRoomCards() {
+    const shownRooms = selectShownRooms();
+    return shownRooms.map((room) => renderGridCard(room.id, <RoomCard room={room} />));
+  }
+
+  function renderGroupJoinCard() {
+    return group.groupJoin && renderGridCard(`group-join-${group.id}`, <GroupJoinCard group={group} />);
+  }
+
+  function renderGroupHeader() {
+    return group.name && <h2 className={classes.title}>{group.name}</h2>;
+  }
+
   return (
     <Box>
-      {group.name && <h2 className={classes.title}>{group.name}</h2>}
+      {renderGroupHeader()}
 
       <Box className={classes.grid}>
-        {rooms.map((room) => (
-          <Box key={room.id} className={classes.card}>
-            <RoomCard room={room} />
-          </Box>
-        ))}
+        {renderGroupJoinCard()}
+        {renderRoomCards()}
       </Box>
     </Box>
   );
