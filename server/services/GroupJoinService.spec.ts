fd2e53cc273d6cf4e9a02b9bd2302fd7484e3c51
Matthias Klass <matthias.klass@tngtech.com>
2020-04-24 14:53:20 +0200

    Add a route for joining a random room

---
+++ server/services/GroupJoinService.spec.ts
@@ -0,0 +1,75 @@
+import { GroupJoinService } from "./GroupJoinService";
+import { OfficeService } from "./OfficeService";
+import { instance, mock, when } from "ts-mockito";
+import { Group } from "../express/types/Group";
+import { RoomWithParticipants } from "../express/types/RoomWithParticipants";
+import { MeetingParticipant } from "../express/types/MeetingParticipant";
+import { v4 as uuid } from "uuid";
+
+describe("GroupJoinService", () => {
+  let groupJoinService: GroupJoinService;
+  let officeService: OfficeService;
+
+  const groupId = "groupId";
+  const minimum = 2;
+
+  const groups: Group[] = [
+    {
+      groupJoin: { minimumParticipantCount: minimum },
+      name: "bla",
+      id: groupId,
+    },
+  ];
+
+  const emptyRoom = roomWithParticipants(0);
+  const roomWithLessThanMinimum = roomWithParticipants(minimum - 1);
+  const roomWithMinimum = roomWithParticipants(minimum);
+  const roomWithMoreThanMinimum = roomWithParticipants(minimum + 1);
+
+  function roomWithParticipants(count: number): RoomWithParticipants {
+    const participants = Array.from(Array(count)).map(() => ({} as MeetingParticipant));
+    return {
+      group: groupId,
+      participants,
+      id: uuid(),
+    } as RoomWithParticipants;
+  }
+
+  beforeEach(() => {
+    officeService = mock(OfficeService);
+    groupJoinService = new GroupJoinService(instance(officeService));
+  });
+
+  it("should fill up rooms below the threshold first", () => {
+    when(officeService.getOffice()).thenReturn({
+      rooms: [emptyRoom, roomWithMoreThanMinimum, roomWithLessThanMinimum, roomWithMinimum],
+      groups,
+    });
+
+    const room = groupJoinService.joinRoomFor(groupId);
+
+    expect(room).toEqual(roomWithLessThanMinimum);
+  });
+
+  it("should start a new room when all services are above the threshold", () => {
+    when(officeService.getOffice()).thenReturn({
+      rooms: [emptyRoom, roomWithMoreThanMinimum, roomWithMinimum],
+      groups,
+    });
+
+    const room = groupJoinService.joinRoomFor(groupId);
+
+    expect(room).toEqual(emptyRoom);
+  });
+
+  it("should start filling rooms above the threshold when no empty room is left", () => {
+    when(officeService.getOffice()).thenReturn({
+      rooms: [roomWithMoreThanMinimum, roomWithMinimum],
+      groups,
+    });
+
+    const room = groupJoinService.joinRoomFor(groupId);
+
+    expect(room).toEqual(roomWithMinimum);
+  });
+});
---
+++ server/services/GroupJoinService.ts
@@ -0,0 +1,48 @@
+import { Service } from "typedi";
+import { minBy, random } from "lodash";
+
+import { Room } from "../express/types/Room";
+import { OfficeService } from "./OfficeService";
+import { RoomWithParticipants } from "../express/types/RoomWithParticipants";
+
+function randomRoomIn(rooms: RoomWithParticipants[]): RoomWithParticipants | undefined {
+  const entry = random(0, rooms.length - 1);
+  return rooms[entry];
+}
+
+@Service()
+export class GroupJoinService {
+  constructor(private readonly officeService: OfficeService) {}
+
+  joinRoomFor(groupId: string): Room | undefined {
+    const office = this.officeService.getOffice();
+    const group = office.groups.find((group) => group.id === groupId);
+    if (!group || !group.groupJoin) {
+      return undefined;
+    }
+
+    const groupRooms = office.rooms.filter((room) => room.group === groupId);
+    const notEmptyRooms = groupRooms.filter((room) => room.participants.length > 0);
+    const availableMinimumCount = minBy(notEmptyRooms, (room) => room.participants.length).participants.length;
+    const roomsWithMinimumParticipantCount = this.roomsWithParticipants(groupRooms, availableMinimumCount);
+
+    // First fill up rooms that are not empty, but have less participants than we configured
+    if (availableMinimumCount > 0 && availableMinimumCount < group.groupJoin.minimumParticipantCount) {
+      return randomRoomIn(roomsWithMinimumParticipantCount);
+    }
+
+    // Then start an empty room
+    const emptyRooms = this.roomsWithParticipants(groupRooms, 0);
+    if (emptyRooms.length > 0) {
+      return randomRoomIn(emptyRooms);
+    }
+
+    // And when no empty rooms are available, but all rooms have participants above the threshold, take
+    // a room with a minimum number of participants.
+    return randomRoomIn(roomsWithMinimumParticipantCount);
+  }
+
+  private roomsWithParticipants(rooms: RoomWithParticipants[], count: number): RoomWithParticipants[] {
+    return rooms.filter((room) => room.participants.length === count);
+  }
+}
