44e9aa5eb1c19c10597dc5760357a99c1645b8bd
Matthias Klass <matthias.klass@tngtech.com>
2020-04-24 09:20:07 +0200

    Repair the search.spec.ts

--- client/src/search.spec.ts
+++ client/src/search.spec.ts
@@ -1,5 +1,6 @@
 import { search } from "./search";
 import { RoomWithParticipants } from "../../server/express/types/RoomWithParticipants";
+import { Office } from "../../server/express/types/Office";
 
 const room1: RoomWithParticipants = {
   id: "1",
@@ -25,58 +26,58 @@ const room2: RoomWithParticipants = {
   joinUrl: "http://bla.blub",
   name: "my second room name",
 };
-const rooms: RoomWithParticipants[] = [room1, room2];
+const office: Office = { groups: [], rooms: [room1, room2] };
 
 describe("search", () => {
   it("should do nothing when no search text was entered", () => {
-    const result = search("", rooms);
+    const result = search("", office);
 
-    expect(result).toEqual(rooms);
+    expect(result).toEqual(office);
   });
 
   describe("filter for room names", () => {
     it("should filter for parts of a room", () => {
-      expect(search("second", rooms)).toEqual([room2]);
+      expect(search("second", office).rooms).toEqual([room2]);
     });
 
     it("should filter for common words", () => {
-      expect(search("my", rooms)).toEqual([room1, room2]);
+      expect(search("my", office).rooms).toEqual([room1, room2]);
     });
 
     it("should filter case insensitive", () => {
-      expect(search("SECOND", rooms)).toEqual([room2]);
+      expect(search("SECOND", office).rooms).toEqual([room2]);
     });
   });
 
   describe("filter for groups", () => {
     it("should filter for parts of a group", () => {
-      expect(search("group", rooms)).toEqual([room1]);
+      expect(search("group", office).rooms).toEqual([room1]);
     });
 
     it("should filter case insensitive", () => {
-      expect(search("GROUP", rooms)).toEqual([room1]);
+      expect(search("GROUP", office).rooms).toEqual([room1]);
     });
   });
 
   describe("filter for participants", () => {
     it("should filter for the name", () => {
-      expect(search("max", rooms)).toEqual([room2]);
+      expect(search("max", office).rooms).toEqual([room2]);
     });
 
     it("should filter for the email address", () => {
-      expect(search("example.com", rooms)).toEqual([room2]);
+      expect(search("example.com", office).rooms).toEqual([room2]);
     });
 
     it("should filter for the id", () => {
-      expect(search("participant_1", rooms)).toEqual([room2]);
+      expect(search("participant_1", office).rooms).toEqual([room2]);
     });
 
     it("should not filter for id parts", () => {
-      expect(search("_1", rooms)).toEqual([]);
+      expect(search("_1", office).rooms).toEqual([]);
     });
 
     it("should not filter for the image URL", () => {
-      expect(search("image.png", rooms)).toEqual([]);
+      expect(search("image.png", office).rooms).toEqual([]);
     });
   });
 });
