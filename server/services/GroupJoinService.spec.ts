940995a30d46d23763cae2678dcd59405fd202ca
Matthias Klass <matthias.klass@tngtech.com>
2020-04-27 14:57:51 +0200

    Reserve spaces on group join (#9)

--- server/services/GroupJoinService.spec.ts
+++ server/services/GroupJoinService.spec.ts
@@ -1,28 +1,33 @@
+import { capture, instance, mock, when } from "ts-mockito";
+import { v4 as uuid } from "uuid";
+import fakeTimers, { InstalledClock } from "@sinonjs/fake-timers";
+
 import { GroupJoinService } from "./GroupJoinService";
 import { OfficeService } from "./OfficeService";
-import { instance, mock, when } from "ts-mockito";
 import { Group } from "../express/types/Group";
 import { RoomWithParticipants } from "../express/types/RoomWithParticipants";
 import { MeetingParticipant } from "../express/types/MeetingParticipant";
-import { v4 as uuid } from "uuid";
+import { RoomsService } from "./RoomsService";
+import { RoomEvent } from "../express/types/RoomEvent";
 
 describe("GroupJoinService", () => {
   let groupJoinService: GroupJoinService;
   let officeService: OfficeService;
+  let roomsService: RoomsService;
 
   const groupId = "groupId";
   const minimum = 2;
 
-  const groups: Group[] = [
-    {
-      groupJoin: {
-        minimumParticipantCount: minimum,
-        description: "Take me somewhere funny",
-      },
-      name: "bla",
-      id: groupId,
+  const group = {
+    groupJoin: {
+      minimumParticipantCount: minimum,
+      description: "Take me somewhere funny",
     },
-  ];
+    name: "bla",
+    id: groupId,
+  };
+
+  const groups: Group[] = [group];
 
   const emptyRoom = roomWithParticipants(0);
   const roomWithLessThanMinimum = roomWithParticipants(minimum - 1);
@@ -34,13 +39,21 @@ describe("GroupJoinService", () => {
     return {
       groupId,
       participants,
-      id: uuid(),
+      id: `${count}__${uuid()}`,
     } as RoomWithParticipants;
   }
 
+  let clock: InstalledClock<any>;
+
+  afterEach(() => {
+    clock.uninstall();
+  });
+
   beforeEach(() => {
+    clock = fakeTimers.install({ now: 1 });
     officeService = mock(OfficeService);
-    groupJoinService = new GroupJoinService(instance(officeService));
+    roomsService = mock(RoomsService);
+    groupJoinService = new GroupJoinService(instance(officeService), instance(roomsService));
   });
 
   it("should fill up rooms below the threshold first", () => {
@@ -96,5 +109,86 @@ describe("GroupJoinService", () => {
     const room = groupJoinService.joinRoomFor(groupId + "a");
 
     expect(room).toBeUndefined();
+  });
+
+  describe("space reservations", () => {
+    it("should consider room reservations when distributing new places", () => {
+      const roomA = roomWithParticipants(1);
+      const roomB = roomWithParticipants(2);
+
+      when(officeService.getOffice()).thenReturn({
+        rooms: [roomA, roomB],
+        groups,
+      });
+
+      groupJoinService.reserveSpaceIn(roomA.id);
+      groupJoinService.reserveSpaceIn(roomA.id);
+
+      const room = groupJoinService.joinRoomFor(groupId);
+
+      expect(room).toEqual(roomB);
+    });
+
+    it("should remove reservations when a participant joins", () => {
+      const room = roomWithParticipants(1);
+      when(officeService.getOffice()).thenReturn({
+        rooms: [room],
+        groups,
+      });
+
+      groupJoinService.reserveSpaceIn(room.id);
+      expect(groupJoinService.participantsInRoom(room)).toEqual(2);
+
+      const listener = capture(roomsService.listenRoomChange).last()[0];
+      (listener as any)({
+        roomId: room.id,
+        type: "join",
+      } as RoomEvent);
+
+      expect(groupJoinService.participantsInRoom(room)).toEqual(1);
+    });
+
+    it("should ignore participant leave events", () => {
+      const room = roomWithParticipants(1);
+      when(officeService.getOffice()).thenReturn({
+        rooms: [room],
+        groups,
+      });
+
+      groupJoinService.reserveSpaceIn(room.id);
+      expect(groupJoinService.participantsInRoom(room)).toEqual(2);
+
+      const listener = capture(roomsService.listenRoomChange).last()[0];
+      (listener as any)({
+        roomId: room.id,
+        type: "leave",
+      } as RoomEvent);
+
+      expect(groupJoinService.participantsInRoom(room)).toEqual(2);
+    });
+
+    it("should remove outdated reservations in a given interval", () => {
+      const room = roomWithParticipants(0);
+      when(officeService.getOffice()).thenReturn({
+        rooms: [room],
+        groups,
+      });
+
+      groupJoinService.reserveSpaceIn(room.id);
+      expect(groupJoinService.participantsInRoom(room)).toEqual(1);
+
+      clock.tick(20000);
+
+      groupJoinService.reserveSpaceIn(room.id);
+      expect(groupJoinService.participantsInRoom(room)).toEqual(2);
+
+      clock.tick(60000);
+
+      expect(groupJoinService.participantsInRoom(room)).toEqual(1);
+
+      clock.tick(30000);
+
+      expect(groupJoinService.participantsInRoom(room)).toEqual(0);
+    });
   });
 });
--- server/services/GroupJoinService.ts
+++ server/services/GroupJoinService.ts
@@ -4,15 +4,36 @@ import { minBy, random } from "lodash";
 import { Room } from "../express/types/Room";
 import { OfficeService } from "./OfficeService";
 import { RoomWithParticipants } from "../express/types/RoomWithParticipants";
+import { RoomsService } from "./RoomsService";
 
 function randomRoomIn(rooms: RoomWithParticipants[]): RoomWithParticipants | undefined {
   const entry = random(0, rooms.length - 1);
   return rooms[entry];
 }
 
+interface ReservedSpaces {
+  [roomId: string]: ReservedSpace[];
+}
+
+interface ReservedSpace {
+  expires: number;
+}
+
+const roomReservationExpiryTimeInMs = 1000 * 60; // 60 seconds
+const cleanupReservationsInterval = 1000 * 30; // 30 seconds;
+
 @Service()
 export class GroupJoinService {
-  constructor(private readonly officeService: OfficeService) {}
+  private reservedSpaces: ReservedSpaces = {};
+
+  constructor(private readonly officeService: OfficeService, roomService: RoomsService) {
+    setInterval(() => this.cleanupReservedSpaces(), cleanupReservationsInterval);
+    roomService.listenRoomChange((event) => {
+      if (event.type === "join") {
+        this.removeReservedSpaceIn(event.roomId);
+      }
+    });
+  }
 
   joinRoomFor(groupId: string): Room | undefined {
     const office = this.officeService.getOffice();
@@ -25,8 +46,9 @@ export class GroupJoinService {
     if (groupRooms.length === 0) {
       return undefined;
     }
-    const notEmptyRooms = groupRooms.filter((room) => room.participants.length > 0);
-    const availableMinimumCount = minBy(notEmptyRooms, (room) => room.participants.length)?.participants?.length || 0;
+    const notEmptyRooms = groupRooms.filter((room) => this.participantsInRoom(room) > 0);
+    const roomWithMinimum = minBy(notEmptyRooms, (room) => this.participantsInRoom(room));
+    const availableMinimumCount = roomWithMinimum ? this.participantsInRoom(roomWithMinimum) : 0;
     const roomsWithMinimumParticipantCount = this.roomsWithParticipants(groupRooms, availableMinimumCount);
 
     // First fill up rooms that are not empty, but have less participants than we configured
@@ -42,10 +64,42 @@ export class GroupJoinService {
 
     // And when no empty rooms are available, but all rooms have participants above the threshold, take
     // a room with a minimum number of participants.
-    return randomRoomIn(roomsWithMinimumParticipantCount);
+    const chosenRoom = randomRoomIn(roomsWithMinimumParticipantCount);
+
+    this.reserveSpaceIn(chosenRoom.id);
+
+    return chosenRoom;
+  }
+
+  reserveSpaceIn(roomId: string) {
+    this.reservedSpaces[roomId] = [
+      ...(this.reservedSpaces[roomId] || []),
+      { expires: new Date().getTime() + roomReservationExpiryTimeInMs },
+    ];
+  }
+
+  private cleanupReservedSpaces() {
+    const now = new Date().getTime();
+    Object.keys(this.reservedSpaces).forEach((roomId) => {
+      this.reservedSpaces[roomId] = this.reservedSpaces[roomId].filter(({ expires }) => now < expires);
+    });
+  }
+
+  private removeReservedSpaceIn(roomId: string) {
+    const spaces = this.reservedSpaces[roomId] || [];
+    const min = minBy(spaces, (space) => space.expires);
+    if (min) {
+      this.reservedSpaces[roomId] = this.reservedSpaces[roomId].filter((space) => space !== min);
+    }
+  }
+
+  participantsInRoom(room: RoomWithParticipants): number {
+    const reserved = this.reservedSpaces[room.id] || [];
+    const participants = room.participants.length;
+    return participants + reserved.length;
   }
 
   private roomsWithParticipants(rooms: RoomWithParticipants[], count: number): RoomWithParticipants[] {
-    return rooms.filter((room) => room.participants.length === count);
+    return rooms.filter((room) => this.participantsInRoom(room) === count);
   }
 }
