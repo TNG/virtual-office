8bda53086a9717539d9060db9001a7ccb2892bca
Andreas Greimel <andreas.greimel@tngtech.com>
2020-04-28 16:45:36 +0200

    Add config option to anonymize participants
    Signed-off-by: Andreas Greimel <andreas.greimel@tngtech.com>

--- client/src/components/RoomCard.tsx
+++ client/src/components/RoomCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Avatar, Button, Card, CardActions, CardContent, CardHeader, Typography } from "@material-ui/core";
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import RoomParticipants from "./RoomParticipants";
 import RoomLinks from "./RoomLinks";
