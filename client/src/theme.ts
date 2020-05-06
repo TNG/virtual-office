d3a2337d446bec6408eb45c25b6600e648683ccf
Andreas Greimel <andreas.greimel@tngtech.com>
2020-05-06 14:57:28 +0200

    BTD ONLY: use system font
    Signed-off-by: Andreas Greimel <andreas.greimel@tngtech.com>

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -11,7 +11,18 @@ let theme = createMuiTheme({
     },
   },
   typography: {
-    fontFamily: "Roboto",
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
   },
   palette: {
     primary: {
