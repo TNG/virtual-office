efc2040295c708532afaed713b812cfdee8a48fe
Matthias Klass <matthias.klass@tngtech.com>
2020-06-11 10:43:45 +0200

    Add the meeting participants on client side if not yet present
    Signed-off-by: Matthias Klass <matthias.klass@tngtech.com>

--- client/src/mapMeetingEventToMeetings.ts
+++ client/src/mapMeetingEventToMeetings.ts
@@ -1,26 +1,36 @@
 import { Meeting } from "../../server/express/types/Meeting";
 import { MeetingEvent } from "../../server/express/types/MeetingEvent";
 
-export function mapMeetingEventToMeetings(meetings: Meeting[], event: MeetingEvent): Meeting[] {
-  function applyEventTo(meeting: Meeting): Meeting {
-    switch (event.type) {
-      case "join":
-        return { ...meeting, participants: [...meeting.participants, event.participant] };
-      case "leave":
-        return {
-          ...meeting,
-          participants: meeting.participants.filter(({ id }) => id !== event.participant.id),
-        };
-      case "update":
-        return {
-          ...meeting,
-          participants: meeting.participants
-            .filter(({ id }) => id !== event.participant.id)
-            .concat([event.participant]),
-        };
-    }
-    return meeting;
+function applyEventTo(meeting: Meeting, event: MeetingEvent): Meeting {
+  switch (event.type) {
+    case "join":
+      const newPart = [...meeting.participants, event.participant];
+      console.log(newPart);
+      return { ...meeting, participants: newPart };
+    case "leave":
+      return {
+        ...meeting,
+        participants: meeting.participants.filter(({ id }) => id !== event.participant.id),
+      };
+    case "update":
+      return {
+        ...meeting,
+        participants: meeting.participants.filter(({ id }) => id !== event.participant.id).concat([event.participant]),
+      };
   }
+  return meeting;
+}
 
-  return meetings.map((meeting) => (meeting.meetingId === event.meetingId ? applyEventTo(meeting) : meeting));
+function addMeetingIfNotYetPresent(meetings: Meeting[], meetingId: string): Meeting[] {
+  if (meetings.find((meeting) => meeting.meetingId === meetingId)) {
+    return meetings;
+  }
+  return [...meetings, { meetingId, participants: [] }];
+}
+
+export function mapMeetingEventToMeetings(meetings: Meeting[], event: MeetingEvent): Meeting[] {
+  const modifiedMeetings = addMeetingIfNotYetPresent(meetings, event.meetingId);
+  return modifiedMeetings.map((meeting) =>
+    meeting.meetingId === event.meetingId ? applyEventTo(meeting, event) : meeting
+  );
 }
