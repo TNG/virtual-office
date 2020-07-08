7bb13831b8f4a25a4de6c6250f9e52912f51a5ad
Andreas Greimel <andreas.greimel@tngtech.com>
2020-07-08 16:04:44 +0200

    Remove unused clientConfig from the office config type and example
    Signed-off-by: Andreas Greimel <andreas.greimel@tngtech.com>

--- server/express/types/ConfigOptions.ts
+++ server/express/types/ConfigOptions.ts
@@ -1,9 +1,7 @@
 import { RoomConfig } from "./Room";
 import { Group } from "./Group";
-import { ClientConfig } from "./ClientConfig";
 
 export interface ConfigOptions {
   rooms: RoomConfig[];
   groups: Group[];
-  clientConfig?: ClientConfig;
 }
