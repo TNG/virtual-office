936211148c13f21928f6813742aacee401870650
Andreas Greimel <andreas.greimel@tngtech.com>
2020-08-21 11:36:20 +0200

    Handle all german umlauts when comparing usernames
    Signed-off-by: Andreas Greimel <andreas.greimel@tngtech.com>

--- server/express/utils/compareableUsername.ts
+++ server/express/utils/compareableUsername.ts
@@ -1,3 +1,10 @@
 export function comparableUsername(username: string) {
-  return username.toLowerCase().replace(/\s/g, "").normalize().replace("ß", "ss");
+  return username
+    .toLowerCase()
+    .replace(/\s/g, "")
+    .normalize()
+    .replace("ß", "ss")
+    .replace("ä", "ae")
+    .replace("ö", "oe")
+    .replace("ü", "ue");
 }
