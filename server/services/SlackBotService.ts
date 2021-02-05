16c66e79a228f91f9e4276287e3a60f2bdc7a143
Andreas Greimel <andreas.greimel@tngtech.com>
2021-02-05 17:23:02 +0100

    Improve SlackBotService to send participant details and update messages in place
    Signed-off-by: Andreas Greimel <andreas.greimel@tngtech.com>

--- server/services/SlackBotService.ts
+++ server/services/SlackBotService.ts
@@ -1,89 +1,61 @@
-import { Service } from "typedi";
-import { ImageElement, WebClient } from "@slack/web-api";
-import { Config } from "../Config";
-import { MeetingsService } from "./MeetingsService";
-import { MeetingEvent } from "../express/types/MeetingEvent";
-import { logger } from "../log";
 import { Block, KnownBlock } from "@slack/types";
-import { OfficeService } from "./OfficeService";
-import { hasSlackNotifications, Room } from "../express/types/Room";
+import { ImageElement, WebClient } from "@slack/web-api";
+import { Service } from "typedi";
+import { Config } from "../Config";
+import { MeetingEvent } from "../express/types/MeetingEvent";
 import { MeetingParticipant } from "../express/types/MeetingParticipant";
-import { MarkRequired } from "ts-essentials";
-
-const LOOP_INTERVAL = 30 * 1000;
+import { hasSlackNotifications, RoomWithSlackNotification } from "../express/types/Room";
+import { logger } from "../log";
+import { MeetingsService } from "./MeetingsService";
+import { OfficeService } from "./OfficeService";
 
 @Service({ multiple: false })
 export class SlackBotService {
-  private readonly slackClient: WebClient | undefined;
+  private readonly slackClient: WebClient;
 
-  private lastNotificationTime: { [roomId: string]: number } = {};
+  private slackMessageIdsPerRoom: { [roomId: string]: string } = {};
 
   constructor(
     config: Config,
     private readonly meetingsService: MeetingsService,
     private readonly officeService: OfficeService
   ) {
-    if (config.slack && config.slack.botOAuthAccessToken) {
-      this.slackClient = new WebClient(config.slack.botOAuthAccessToken);
-      this.meetingsService.listenParticipantsChange((event) => this.onRoomEvent(event));
-
-      setInterval(() => this.sendRecurringNotification(), LOOP_INTERVAL);
+    if (!(config.slack && config.slack.botOAuthAccessToken)) {
+      throw Error("No slack config found");
     }
+
+    this.slackClient = new WebClient(config.slack.botOAuthAccessToken);
+    this.meetingsService.listenParticipantsChange((event) => this.onRoomEvent(event));
   }
 
   private onRoomEvent(event: MeetingEvent) {
-    this.officeService.getRoomsForMeetingId(event.meetingId).map((room) => this.handleMeetingEventForRoom(event, room));
+    this.officeService
+      .getRoomsForMeetingId(event.meetingId)
+      .filter(hasSlackNotifications)
+      .map((room) =>
+        this.sendSlackNotificationOnMeetingEvent(event, room).catch((error) =>
+          logger.error("Failed to send slack notification", error)
+        )
+      );
   }
 
-  private handleMeetingEventForRoom(event: MeetingEvent, room: Room) {
-    const participants = this.meetingsService.getParticipantsIn(room.roomId).length;
+  private async sendSlackNotificationOnMeetingEvent(event: MeetingEvent, room: RoomWithSlackNotification) {
+    const participants = this.meetingsService.getParticipantsIn(room.meetingId);
     const slackNotification = room.slackNotification;
     logger.info(
       `Slack message roomEvent for room=${
         room.roomId
-      }, roomFound=${!!room}, slackNotification=${!!slackNotification}, participants=${participants}, eventType=${
-        event.type
-      }`
+      }, roomFound=${!!room}, slackNotification=${!!slackNotification}, participants=${
+        participants.length
+      }, eventType=${event.type}`
     );
 
-    if (event.type === "join") {
-      if (slackNotification && participants === 1) {
-        this.sendMessageToRoom(room, [
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: `*${room.name}* is occupied - <${room.joinUrl}|Join>`,
-            },
-          },
-        ]);
-      }
-    } else if (event.type === "leave") {
-      if (slackNotification && participants === 0) {
-        this.sendMessageToRoom(room, [
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: `*${room.name}* is empty - <${room.joinUrl}|Join>`,
-            },
-          },
-        ]);
-      }
+    if (event.type === "join" || event.type === "leave") {
+      await this.sendParticipantUpdate(room, participants);
     }
   }
 
-  private sendRecurringNotification() {
-    const rooms = this.officeService.getOffice().rooms;
-    rooms.forEach((room) => {
-      const participants = this.meetingsService.getParticipantsIn(room.roomId);
-      if (hasSlackNotifications(room) && participants.length > 0 && this.shouldSendRecurringNotification(room)) {
-        this.sendParticipantUpdate(room, participants);
-      }
-    });
-  }
-
-  private sendParticipantUpdate(room: Room, participants: MeetingParticipant[]) {
+  private async sendParticipantUpdate(room: RoomWithSlackNotification, participants: MeetingParticipant[]) {
     const imageElements: ImageElement[] = participants
       .filter((participant) => participant.imageUrl)
       .map((participant) => ({
@@ -92,15 +64,18 @@ export class SlackBotService {
         alt_text: participant.username,
       }));
 
-    this.sendMessageToRoom(room, [
+    const blocks: KnownBlock[] = [
       {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: `*${room.name}* is occupied - <${room.joinUrl}|Join>`,
+          text: `*${room.name}* is ${participants.length > 0 ? "occupied" : "empty"} - <${room.joinUrl}|Join>`,
         },
       },
-      {
+    ];
+
+    if (participants.length > 0) {
+      blocks.push({
         type: "context",
         elements: [
           ...imageElements,
@@ -109,34 +84,42 @@ export class SlackBotService {
             text: `${participants.length} participant${participants.length > 1 ? "s" : ""}`,
           },
         ],
-      },
-    ]);
-  }
-
-  private sendMessageToRoom(room: Room, blocks: (KnownBlock | Block)[]) {
-    this.lastNotificationTime[room.roomId] = Date.now();
-
-    (async () => {
-      if (!this.slackClient || !room.slackNotification) {
-        return;
-      }
-
-      const res = await this.slackClient.chat.postMessage({
-        channel: room.slackNotification.channelId,
-        text: "",
-        blocks,
       });
+    }
+    const slackMessageId = this.slackMessageIdsPerRoom[room.roomId]
+      ? await this.updateMessageForRoom(room, blocks)
+      : await this.sendMessageToRoom(room, blocks);
 
-      // `res` contains information about the posted message
-      logger.info("Slack message sent", res.ts);
-    })();
+    if (participants.length > 0) {
+      this.slackMessageIdsPerRoom[room.roomId] = slackMessageId;
+    } else {
+      delete this.slackMessageIdsPerRoom[room.roomId];
+    }
   }
 
-  private shouldSendRecurringNotification(room: MarkRequired<Room, "slackNotification">) {
-    return (
-      room.slackNotification.notificationInterval &&
-      (!this.lastNotificationTime[room.roomId] ||
-        Date.now() - this.lastNotificationTime[room.roomId] > room.slackNotification.notificationInterval * 60 * 1000)
-    );
+  private async sendMessageToRoom(room: RoomWithSlackNotification, blocks: (KnownBlock | Block)[]): Promise<string> {
+    const res = await this.slackClient.chat.postMessage({
+      channel: room.slackNotification.channelId,
+      text: "",
+      blocks,
+    });
+
+    return res.ts as string;
+  }
+
+  private async updateMessageForRoom(room: RoomWithSlackNotification, blocks: (KnownBlock | Block)[]): Promise<string> {
+    const existingSlackMessageId = this.slackMessageIdsPerRoom[room.roomId];
+    if (!existingSlackMessageId) {
+      throw Error(`No slack message found for room ${room.roomId}`);
+    }
+
+    const res = await this.slackClient.chat.update({
+      ts: existingSlackMessageId,
+      channel: room.slackNotification.channelId,
+      text: "",
+      blocks,
+    });
+
+    return res.ts as string;
   }
 }
