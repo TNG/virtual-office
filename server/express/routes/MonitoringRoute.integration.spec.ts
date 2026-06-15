409aec00f81e8df1d14f160ac912f63ce5c8b126
Sebastian Zett <sebastian.zett@tngtech.com>
2026-06-15 16:18:50 +0200

    test: add unit tests for ensureLoggedIn middleware
    Signed-off-by: Sebastian Zett <sebastian.zett@tngtech.com>

---
+++ server/express/routes/MonitoringRoute.integration.spec.ts
@@ -0,0 +1,30 @@
+import "reflect-metadata";
+
+import { Container } from "typedi";
+import { ConfigOptions } from "../types/ConfigOptions";
+import { startTestServerWithConfig, TestServer } from "../../testUtils/startTestServerWithConfig";
+import request from "supertest";
+
+const config: ConfigOptions = {
+  rooms: [],
+  groups: [],
+};
+
+describe("MonitoringRoute", () => {
+  let server: TestServer;
+
+  beforeEach(async () => {
+    server = await startTestServerWithConfig(config);
+  });
+
+  afterEach(() => {
+    Container.reset();
+  });
+
+  it("should return 200 and OK on GET /api/monitoring/health", async () => {
+    const response = await request(server.app).get("/api/monitoring/health");
+
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("OK");
+  });
+});
