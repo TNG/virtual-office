adc6bd6c8201b3b966447944a0d1405db0a51e8d
Sebastian Zett <sebastian.zett@tngtech.com>
2026-06-15 17:04:16 +0200

    test: add component smoke tests for Login
    Signed-off-by: Sebastian Zett <sebastian.zett@tngtech.com>

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|svg)$": "<rootDir>/src/__mocks__/fileMock.js",
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json",
+      diagnostics: {
+        errorCodes: ["TS1343"],
+      },
+    },
+  },
 };
