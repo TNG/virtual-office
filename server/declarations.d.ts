532bcaaacb32082184b933ca33b50524b7c35230
Sebastian Zett <sebastian.zett@tngtech.com>
2026-06-16 07:55:31 +0200

    feat: upgrade luxon 3, @slack/web-api 7, swagger-ui-express 5, supertest 7
    Signed-off-by: Sebastian Zett <sebastian.zett@tngtech.com>

--- server/declarations.d.ts
+++ server/declarations.d.ts
@@ -2,7 +2,3 @@ declare module "passport-slack-oauth2";
 declare module "dotenv" {
   export function config(options?: any): any;
 }
-declare module "axios" {
-  const axios: any;
-  export default axios;
-}
