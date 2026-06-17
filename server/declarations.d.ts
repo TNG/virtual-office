adca110a3d0b2eb0d35b297691c7ba78e445435d
Sebastian Zett <sebastian.zett@tngtech.com>
2026-06-17 15:48:20 +0200

    fix: add swagger-ui-express module declaration for pnpm strict resolution
    Signed-off-by: Sebastian Zett <sebastian.zett@tngtech.com>

--- server/declarations.d.ts
+++ server/declarations.d.ts
@@ -2,3 +2,10 @@ declare module "passport-slack-oauth2";
 declare module "dotenv" {
   export function config(options?: any): any;
 }
+declare module "swagger-ui-express" {
+  import { RequestHandler } from "express";
+  const swaggerUI: { serve: RequestHandler[]; setup: (options?: any) => RequestHandler };
+  export default swaggerUI;
+  export const serve: RequestHandler[];
+  export const setup: (options?: any) => RequestHandler;
+}
